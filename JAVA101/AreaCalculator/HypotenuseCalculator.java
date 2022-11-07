22371d60e94db9bd67a8338c7ecdce1e160e574d
umut <113436899+CoderVega@users.noreply.github.com>
2022-11-07 13:23:48 +0300

    Add files via upload

---
+++ JAVA101/AreaCalculator/HypotenuseCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class HypotenuseCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give the first side: ");
+        double a = input.nextDouble();
+        System.out.println("Give the second side: ");
+        double b = input.nextDouble();
+        double hypotenuse = Math.sqrt((a*a)+(b*b));
+        System.out.println("Hypotenuse is equals to: "+hypotenuse);
+
+    }
+}
