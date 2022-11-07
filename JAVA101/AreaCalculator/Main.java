32336c1b3aa21ddc76d7556b5752920a99a7a79b
umut <113436899+CoderVega@users.noreply.github.com>
2022-11-07 15:40:27 +0300

    www.Patika.dev

---
+++ JAVA101/AreaCalculator/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double r, area, perimeter ;
+        System.out.println("Give the radius: ");
+        r = input.nextDouble();
+        area = Math.PI * ((r * r )/4);
+        perimeter = 2*Math.PI*(r/2);
+        System.out.println("Area of Circle is: "+ area +"\n"+"Perimeter of the circle is: "+perimeter);
+    }
+}
