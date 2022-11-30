3ebc362efdd7c47fe1b70be1b1f8902856afc34d
umut <113436899+OkcuYazilimci@users.noreply.github.com>
2022-11-30 19:49:09 +0300

    www.Patika.dev

---
+++ JAVA101/OOP/Student System/Salary Calculator/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Umut UYGUN",15000,45,2017);
+        Employee employee2 = new Employee("Zahara BENNET",4000,20,1998);
+        Employee employee3 = new Employee("Aslan ÇERGİ",13000,30,2005);
+        employee1.toPrint();
+        employee2.toPrint();
+        employee3.toPrint();
+    }
+}
